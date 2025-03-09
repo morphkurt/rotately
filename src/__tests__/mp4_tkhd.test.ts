9615b6e39b578f90042611fea0d6e4cdd16e7bd5
damitha <damitha.gunawardena@gmail.com>
2025-03-09 21:23:14 +1100

    .

--- src/__tests__/mp4_tkhd.test.ts
+++ src/__tests__/mp4_tkhd.test.ts
@@ -1,4 +1,4 @@
-import { findVideoTkhdOffset, modifyTkhdMatrix, calculateMatrixOffset } from '../mp4_tkhd';
+import { findVideoTkhdOffset, modifyTkhdMatrix, calculateMatrixOffset, IDENTITY_MATRIX, ROTATE_180, ROTATE_270, ROTATE_90 } from '../mp4_tkhd';
 import fs from 'fs';
 import path from 'path';
 
@@ -44,71 +44,64 @@ describe('MP4 TKHD operations', () => {
   }
 
   describe("modifyTkhdMatrix", () => {
-    let fileBuffer: Uint8Array;
+    let identyMatrixBuffer: Uint8Array;
+    let rotate90MatrixBuffer: Uint8Array;
+    let rotate180MatrixBuffer: Uint8Array;
+    let rotate270MatrixBuffer: Uint8Array;
+
     const matrixOffset = 100;
 
     beforeEach(() => {
-      fileBuffer = createFileBuffer(200);
+      identyMatrixBuffer = createFileBuffer(200);
+      rotate90MatrixBuffer = createFileBuffer(200);
+      rotate180MatrixBuffer = createFileBuffer(200);
+      rotate270MatrixBuffer = createFileBuffer(200);
+      identyMatrixBuffer.set(IDENTITY_MATRIX,100);
+      rotate90MatrixBuffer.set(ROTATE_90,100);
+      rotate180MatrixBuffer.set(ROTATE_180,100);
+      rotate270MatrixBuffer.set(ROTATE_270,100);
     });
 
     test("should modify file buffer for 90cw rotation", () => {
-      modifyTkhdMatrix(fileBuffer, matrixOffset, "90cw");
+      modifyTkhdMatrix(identyMatrixBuffer, matrixOffset, IDENTITY_MATRIX,"90cw");
+      expect(identyMatrixBuffer.slice(matrixOffset, matrixOffset + 36)).toEqual(ROTATE_90);
+      
+      modifyTkhdMatrix(rotate90MatrixBuffer, matrixOffset, ROTATE_90, "90cw");
+      expect(rotate90MatrixBuffer.slice(matrixOffset, matrixOffset + 36)).toEqual(ROTATE_180);
 
-      expect(fileBuffer.slice(matrixOffset, matrixOffset + 36)).toEqual(new Uint8Array([
-        0x00, 0x00, 0x00, 0x00,
-        0x00, 0x01, 0x00, 0x00,
-        0x00, 0x00, 0x00, 0x00,
+      modifyTkhdMatrix(rotate180MatrixBuffer, matrixOffset,ROTATE_180, "90cw");
+      expect(rotate180MatrixBuffer.slice(matrixOffset, matrixOffset + 36)).toEqual(ROTATE_270);
 
-        0xFF, 0xFF, 0x00, 0x00,
-        0x00, 0x00, 0x00, 0x00,
-        0x00, 0x00, 0x00, 0x00,
-
-        0x00, 0x00, 0x00, 0x00,
-        0x00, 0x00, 0x00, 0x00,
-        0x40, 0x00, 0x00, 0x00
-      ]));
+      modifyTkhdMatrix(rotate270MatrixBuffer, matrixOffset,ROTATE_270, "90cw");
+      expect(rotate270MatrixBuffer.slice(matrixOffset, matrixOffset + 36)).toEqual(IDENTITY_MATRIX);
     });
 
     test("should modify file buffer for 90ccw rotation", () => {
-      modifyTkhdMatrix(fileBuffer, matrixOffset, "90ccw");
+      modifyTkhdMatrix(identyMatrixBuffer, matrixOffset, IDENTITY_MATRIX,"90ccw");
+      expect(identyMatrixBuffer.slice(matrixOffset, matrixOffset + 36)).toEqual(ROTATE_270);
+      
+      modifyTkhdMatrix(rotate90MatrixBuffer, matrixOffset, ROTATE_90, "90ccw");
+      expect(rotate90MatrixBuffer.slice(matrixOffset, matrixOffset + 36)).toEqual(IDENTITY_MATRIX);
 
-      expect(fileBuffer.slice(matrixOffset, matrixOffset + 36)).toEqual(new Uint8Array([
-        0x00, 0x00, 0x00, 0x00,
-        0xFF, 0xFF, 0x00, 0x00,
-        0x00, 0x00, 0x00, 0x00,
+      modifyTkhdMatrix(rotate180MatrixBuffer, matrixOffset,ROTATE_180, "90ccw");
+      expect(rotate180MatrixBuffer.slice(matrixOffset, matrixOffset + 36)).toEqual(ROTATE_90);
 
-        0x00, 0x01, 0x00, 0x00,
-        0x00, 0x00, 0x00, 0x00,
-        0x00, 0x00, 0x00, 0x00,
-
-        0x00, 0x00, 0x00, 0x00,
-        0x00, 0x00, 0x00, 0x00,
-        0x40, 0x00, 0x00, 0x00
-      ]));
+      modifyTkhdMatrix(rotate270MatrixBuffer, matrixOffset,ROTATE_270, "90ccw");
+      expect(rotate270MatrixBuffer.slice(matrixOffset, matrixOffset + 36)).toEqual(ROTATE_180);
     });
 
     test("should modify file buffer for 180 rotation", () => {
-      modifyTkhdMatrix(fileBuffer, matrixOffset, "180");
+      modifyTkhdMatrix(identyMatrixBuffer, matrixOffset, IDENTITY_MATRIX,"180");
+      expect(identyMatrixBuffer.slice(matrixOffset, matrixOffset + 36)).toEqual(ROTATE_180);
+      
+      modifyTkhdMatrix(rotate90MatrixBuffer, matrixOffset, ROTATE_90, "180");
+      expect(rotate90MatrixBuffer.slice(matrixOffset, matrixOffset + 36)).toEqual(ROTATE_270);
 
-      expect(fileBuffer.slice(matrixOffset, matrixOffset + 36)).toEqual(new Uint8Array([
-        0xFF, 0xFF, 0x00, 0x00,
-        0x00, 0x00, 0x00, 0x00,
-        0x00, 0x00, 0x00, 0x00,
+      modifyTkhdMatrix(rotate180MatrixBuffer, matrixOffset,ROTATE_180, "180");
+      expect(rotate180MatrixBuffer.slice(matrixOffset, matrixOffset + 36)).toEqual(IDENTITY_MATRIX);
 
-        0x00, 0x00, 0x00, 0x00,
-        0xFF, 0xFF, 0x00, 0x00,
-        0x00, 0x00, 0x00, 0x00,
-
-        0x00, 0x00, 0x00, 0x00,
-        0x00, 0x00, 0x00, 0x00,
-        0x40, 0x00, 0x00, 0x00
-      ]));
-    });
-
-    test("should not modify file buffer if offset is out of bounds", () => {
-      modifyTkhdMatrix(fileBuffer, 180, "90cw");
-
-      expect(fileBuffer).toEqual(createFileBuffer(200));
+      modifyTkhdMatrix(rotate270MatrixBuffer, matrixOffset,ROTATE_270, "180");
+      expect(rotate270MatrixBuffer.slice(matrixOffset, matrixOffset + 36)).toEqual(ROTATE_90);
     });
   });
 
